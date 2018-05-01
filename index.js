f415b08da7d8aa5d2a977445b0e900495f455429
db001 <dnbennett@hotmail.co.uk>
2018-05-01 22:47:12 +0100

    Adding data to database

--- index.js
+++ index.js
@@ -7,7 +7,39 @@ require('dotenv').config();
 
 const app = express();
 
-const routes = require('./routes/routes')
+const routes = require('./routes/routes');
+
+// Firebase setup
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccount.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://url-sharer-98088.firebaseio.com/'
+});
+
+const db = admin.database();
+const ref = db.ref("/");
+ref.once("value", function (snapshot) {
+    console.log(snapshot.val());
+}, function (errorObject) {
+    console.log(`The read failed: ${errorObject.code}`);
+});
+
+const usersRef = ref.child('users');
+usersRef.update({
+    bob: {
+        email: "bob@test.co.uk",
+        firstName: "Mikey"
+    },
+    julio: {
+        email: "jman@example.com",
+        firstName: "Julio"
+    }
+});
+// usersRef.update({
+//     "bob/firstName": "bobby"
+// });
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
