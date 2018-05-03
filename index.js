82c0822221503d18aa88347c9f8c291686eb581f
db001 <dnbennett@hotmail.co.uk>
2018-05-03 22:13:22 +0100

    Reading and displaying data from database

--- index.js
+++ index.js
@@ -9,34 +9,35 @@ const app = express();
 
 const routes = require('./routes/routes');
 
-// Firebase setup
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccount.json');
+// // Firebase setup
+// const admin = require('firebase-admin');
+// const serviceAccount = require('./serviceAccount.json');
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://url-sharer-98088.firebaseio.com/'
-});
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: 'https://url-sharer-98088.firebaseio.com/'
+// });
 
-const db = admin.database();
-const ref = db.ref("/");
-ref.once("value", function (snapshot) {
-    console.log(snapshot.val());
-}, function (errorObject) {
-    console.log(`The read failed: ${errorObject.code}`);
-});
+// const db = admin.database();
+// const ref = db.ref("/");
+// ref.once("value", function (snapshot) {
+//     console.log(snapshot.val());
+// }, function (errorObject) {
+//     console.log(`The read failed: ${errorObject.code}`);
+// });
 
-const usersRef = ref.child('users');
-usersRef.update({
-    bob: {
-        email: "bob@test.co.uk",
-        firstName: "Mikey"
-    },
-    julio: {
-        email: "jman@example.com",
-        firstName: "Julio"
-    }
-});
+// const usersRef = ref.child('users');
+// usersRef.update({
+//     bob: {
+//         email: "bob@test.co.uk",
+//         firstName: "Mikey"
+//     },
+//     julio: {
+//         email: "jman@example.com",
+//         firstName: "Julio"
+//     }
+// });
+
 // usersRef.update({
 //     "bob/firstName": "bobby"
 // });
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+// Firebase setup
+const admin = require('firebase-admin');
+const serviceAccount = require('../serviceAccount.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://url-sharer-98088.firebaseio.com/'
+});
+
+const db = admin.database();
+const ref = db.ref("/");
+
 router.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Home'
+    ref.once("value", function (snapshot) {
+        const users = snapshot.val();
+        console.log(users);
+        res.render('index', {
+            title: 'Home',
+            ...users
+        });
+    }, function (errorObject) {
+        console.log(`The read failed: ${errorObject.code}`);
     });
 });
 
